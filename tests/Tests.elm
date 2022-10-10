c8b410658d359be0b56dd764f2c8fa8cc9b7ba43
Bob Grudem <bobgru@hotmail.com>
2022-10-10 08:13:44 -0400

    First unit tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,7 +1,11 @@
 module Tests exposing (..)
 
-import Test exposing (..)
 import Expect
+import Grid as Grid
+import Location exposing (Location)
+import Set as Set
+import Test exposing (..)
+
 
 
 -- Check out https://package.elm-lang.org/packages/elm-explorations/test/latest to learn more about testing in Elm!
@@ -10,13 +14,175 @@ import Expect
 all : Test
 all =
     describe "A Test Suite"
-        [ test "Addition" <|
+        [ test "getConnectedCells horizontally" <|
             \_ ->
-                Expect.equal 10 (3 + 7)
-        , test "String.left" <|
+                let
+                    game : Grid.SparseGridInput
+                    game =
+                        { width = 2
+                        , height = 2
+                        , cells =
+                            [ ( ( 0, 0 ), Grid.white )
+                            , ( ( 0, 1 ), Grid.white )
+                            ]
+                        }
+
+                    sparse =
+                        Grid.initialModel game
+
+                    expected : Set.Set Location
+                    expected =
+                        Set.fromList [ ( 0, 0 ), ( 0, 1 ) ]
+                in
+                Expect.equal expected (Grid.getConnectedCells (Grid.matchColor Grid.white) sparse.grid.cells ( 0, 0 ))
+        , test "getConnectedCells vertically" <|
             \_ ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
+                let
+                    game : Grid.SparseGridInput
+                    game =
+                        { width = 2
+                        , height = 2
+                        , cells =
+                            [ ( ( 0, 0 ), Grid.white )
+                            , ( ( 1, 0 ), Grid.white )
+                            ]
+                        }
+
+                    sparse =
+                        Grid.initialModel game
+
+                    expected : Set.Set Location
+                    expected =
+                        Set.fromList [ ( 0, 0 ), ( 1, 0 ) ]
+                in
+                Expect.equal expected (Grid.getConnectedCells (Grid.matchColor Grid.white) sparse.grid.cells ( 0, 0 ))
+        , test "getConnectedCells not diagonally, and not if singleton" <|
             \_ ->
-                Expect.fail "failed as expected!"
+                let
+                    game : Grid.SparseGridInput
+                    game =
+                        { width = 2
+                        , height = 2
+                        , cells =
+                            [ ( ( 0, 0 ), Grid.white )
+                            , ( ( 1, 1 ), Grid.white )
+                            ]
+                        }
+
+                    sparse =
+                        Grid.initialModel game
+
+                    expected : Set.Set Location
+                    expected =
+                        Set.empty
+                in
+                Expect.equal expected (Grid.getConnectedCells (Grid.matchColor Grid.white) sparse.grid.cells ( 0, 0 ))
+        , test "getConnectedCells vertically and horizontally" <|
+            \_ ->
+                let
+                    game : Grid.SparseGridInput
+                    game =
+                        { width = 4
+                        , height = 4
+                        , cells =
+                            [ ( ( 0, 0 ), Grid.white )
+                            , ( ( 0, 1 ), Grid.white )
+                            , ( ( 0, 2 ), Grid.white )
+                            , ( ( 0, 3 ), Grid.white )
+                            , ( ( 1, 0 ), Grid.white )
+                            , ( ( 1, 3 ), Grid.white )
+                            , ( ( 2, 0 ), Grid.white )
+                            , ( ( 3, 0 ), Grid.white )
+                            , ( ( 3, 1 ), Grid.white )
+                            ]
+                        }
+
+                    sparse =
+                        Grid.initialModel game
+
+                    expected : Set.Set Location
+                    expected =
+                        Set.fromList [ ( 0, 0 ), ( 0, 1 ), ( 0, 2 ), ( 0, 3 ), ( 1, 0 ), ( 1, 3 ), ( 2, 0 ), ( 3, 0 ), ( 3, 1 ) ]
+                in
+                Expect.equal expected (Grid.getConnectedCells (Grid.matchColor Grid.white) sparse.grid.cells ( 0, 0 ))
+        , test "getConnectedComplements when no errors" <|
+            \_ ->
+                let
+                    game : Grid.SparseGridInput
+                    game =
+                        { width = 4
+                        , height = 4
+                        , cells =
+                            [ ( ( 0, 0 ), Grid.white )
+                            , ( ( 0, 1 ), Grid.white )
+                            , ( ( 0, 2 ), Grid.white )
+                            , ( ( 0, 3 ), Grid.white )
+                            , ( ( 1, 0 ), Grid.white )
+                            , ( ( 1, 3 ), Grid.white )
+                            , ( ( 2, 0 ), Grid.white )
+                            , ( ( 3, 0 ), Grid.white )
+                            , ( ( 3, 1 ), Grid.white )
+                            ]
+                        }
+
+                    sparse =
+                        Grid.initialModel game
+
+                    connectedCells : Set.Set Location
+                    connectedCells =
+                        Set.fromList [ ( 0, 0 ), ( 0, 1 ), ( 0, 2 ), ( 0, 3 ), ( 1, 0 ), ( 1, 3 ), ( 2, 0 ), ( 3, 0 ), ( 3, 1 ) ]
+
+                    expected : List (Set.Set Location)
+                    expected =
+                        [ Set.fromList [ ( 1, 1 ), ( 1, 2 ), ( 2, 1 ), ( 2, 2 ), ( 2, 3 ), ( 3, 2 ), ( 3, 3 ) ] ]
+                in
+                Expect.equal expected (Grid.getConnectedComplements connectedCells sparse.grid)
+        , test "getConnectedComplements when checkerboard formed" <|
+            \_ ->
+                let
+                    game : Grid.SparseGridInput
+                    game =
+                        { width = 4
+                        , height = 4
+                        , cells =
+                            [ ( ( 0, 0 ), Grid.white )
+                            , ( ( 0, 1 ), Grid.white )
+                            , ( ( 0, 2 ), Grid.white )
+                            , ( ( 0, 3 ), Grid.white )
+                            , ( ( 1, 0 ), Grid.white )
+                            , ( ( 1, 3 ), Grid.white )
+                            , ( ( 2, 0 ), Grid.white )
+                            , ( ( 2, 3 ), Grid.white )
+                            , ( ( 3, 0 ), Grid.white )
+                            , ( ( 3, 1 ), Grid.white )
+                            , ( ( 3, 2 ), Grid.white )
+                            ]
+                        }
+
+                    sparse =
+                        Grid.initialModel game
+
+                    connectedCells : Set.Set Location
+                    connectedCells =
+                        Set.fromList
+                            [ ( 0, 0 )
+                            , ( 0, 1 )
+                            , ( 0, 2 )
+                            , ( 0, 3 )
+                            , ( 1, 0 )
+                            , ( 1, 3 )
+                            , ( 2, 0 )
+                            , ( 2, 3 )
+                            , ( 3, 0 )
+                            , ( 3, 1 )
+                            , ( 3, 2 )
+                            ]
+
+                    expected : List (Set.Set Location)
+                    expected =
+                        [ Set.fromList [ ( 3, 3 ) ]
+                        , Set.fromList [ ( 1, 1 ), ( 1, 2 ), ( 2, 1 ), ( 2, 2 ) ]
+                        ]
+                in
+                Expect.equal expected (Debug.log "result" Grid.getConnectedComplements connectedCells sparse.grid)
         ]
